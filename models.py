3fb25ebaad534e7081d4576e3c2ce7813a573450
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-12-05 18:10:49 +0200

    3 redone db in order to bind Product and Properties as One-To-One

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 import datetime
 
-from sqlalchemy import Column, Integer, String, Float, Boolean, UniqueConstraint, DATE
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import (Column, Integer, String, Float, Boolean,
+                        UniqueConstraint, ForeignKey, ForeignKeyConstraint)
+from sqlalchemy.orm import DeclarativeBase, relationship
 
 from database import Base, engine
 
@@ -14,6 +15,7 @@ class Product(Base):
     id = Column(Integer, primary_key=True, index=True)
     model = Column(String, unique=True)
     category = Column(String)
+    property = relationship('Property', uselist=False)
 
     __table_args__ = (
         UniqueConstraint('model', name='uq_product_model'),  # Named unique constraint
@@ -26,5 +28,12 @@ class Property(Base):
     price = Column(Float, nullable=False)
     is_available = Column(Boolean, name='availability', default=True)
     description = Column(String(100), nullable=True)
+    product_id = Column(Integer, nullable=False)
+
+    __table_args__ = (
+        ForeignKeyConstraint(
+            ['product_id'], ['products.id'], name='fk_properties_product_id'
+        ),
+    )
 
 Base.metadata.create_all(bind=engine)
