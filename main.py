1496893adbf45fbaeee06f27bad6f59593c4a9fd
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-12-16 08:24:12 +0200

    3 created docker-compose.yml for run fastapi with depedency redis

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 import uvicorn
+from redis import Redis, ConnectionPool
 
 app = FastAPI()
+
+pool = ConnectionPool(host='redis', port=6379, db=0)
+r = Redis(connection_pool=pool)
+
 
 @app.get("/", response_class=RedirectResponse, include_in_schema=False)
 def read_root():
     return RedirectResponse(url='/docs')
 
 @app.get("/items/{item_id}")
-def read_item(item_id: int, q: str = None):
-    return {"item_id": item_id, "q": q}
+def read_item(id: int, q: str = None):
+
+    cached_value = r.get(f'item_{id}')
+    if not cached_value:
+        r.set(f'item_{id}', q or 'No Query')
+        cached_value = r.get(f'item_{id}')
+
+    return {"item_id": id, "q": cached_value}
+
+@app.get("/health")
+async def health_check():
+    return {"status": "healthy"}
 
 if __name__ == '__main__':
     uvicorn.run(app=app, host='127.0.0.1', port=8000)
