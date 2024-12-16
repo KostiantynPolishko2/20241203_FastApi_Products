b986d05eb33ee35dfd47f14beda2956ab53bfa3c
Kostiantyn Polishko <polxs_wp31@student.itstep.org>
2024-12-16 06:14:36 +0200

    1 created default fastapi

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/", response_class=RedirectResponse, include_in_schema=False)
+def read_root():
+    return RedirectResponse(url='/docs')
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str = None):
+    return {"item_id": item_id, "q": q}
+
+if __name__ == '__main__':
+    uvicorn.run(app=app, http='httptools', host='127.0.0.1', port=8081)
